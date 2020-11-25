bf01491378c516cbf807b5682e8c9f364ba7f3dd
Loren Cipriano <loren25cipriano@gmail.com>
2020-11-25 14:34:30 -0800

    commit tree a52745c149969c6dc9f04524ccc5e90755926577

---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+    def talk
+        puts "Hello World!"
+    end 
+    def walk
+        puts "The Person is walking"
+    end 
+end 
+
+# Person
+#   is defined
+#   #talk
+#     responds to the #talk instance method
+#     puts "Hello World!"
+#   #walk
+#     responds to the #walk instance method
+#     puts "The Person is walking"
